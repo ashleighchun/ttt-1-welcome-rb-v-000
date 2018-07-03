27ec1e7a0b5cad830b3eeedbc898956f756bec35
Ashleigh Chun <ashleigh.chun@wework.com>
2018-07-03 04:56:30 +0000

    automatically backed up by learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,3 +1,3 @@
 puts "Welcome to Tic Tac Toe!"
-puts "You can see what this file does by running:"
-puts "ruby lib/welcome.rb from your terminal."
+# You can see what this file does by running:
+# ruby lib/welcome.rb from your terminal.
